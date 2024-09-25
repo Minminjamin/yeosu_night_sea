fc1e7a1762775bff0f5d46a895449ddb177d903f
Minminjamin <0429el@gmail.com>
2024-09-25 23:54:45 +0900

    style : prettier 설치 및 재적용 확인

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,16 @@
 <script>
-	import Lyrics from "./view/Lyrics.svelte";
+    import Lyrics from "./view/Lyrics.svelte";
 </script>
 
-<main>
-	<Lyrics/>
-
-</main>
-
 <style lang="scss">
-	main {
-		width: 100%;
-		height: 100%;
-		background: #6f67ff;
-		color: #fff;
-	}
+    main {
+        width: 100%;
+        height: 100%;
+        background: #6f67ff;
+        color: #fff;
+    }
 </style>
+
+<main>
+    <Lyrics />
+</main>
